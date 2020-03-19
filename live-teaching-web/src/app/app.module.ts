5b706f287315846d8359c02e5ae9ee2f5f03d24f
methanick <nick.metasit@gmail.com>
2020-03-19 16:31:05 +0700

    [Update] Login - update UI in login page

--- live-teaching-web/src/app/app.module.ts
+++ live-teaching-web/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { NavbarComponent } from "./navbar/navbar.component";
 
 import { ButtonModule } from "primeng/button";
 import { MenubarModule } from "primeng/menubar";
+import { CardModule } from "primeng/card";
 @NgModule({
   declarations: [
     AppComponent,
@@ -36,7 +37,8 @@ import { MenubarModule } from "primeng/menubar";
     FormsModule,
     ReactiveFormsModule,
     ButtonModule,
-    MenubarModule
+    MenubarModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
