51c4927df2cbca20cca4139403fa6778f1305aa6
Thanaphon Toprasit <thanaphon@bonmek.com>
2020-03-20 17:09:14 +0700

    fixed style

--- live-teaching-web/src/app/student/student.component.ts
+++ live-teaching-web/src/app/student/student.component.ts
@@ -82,4 +82,9 @@ export class StudentComponent implements OnInit, OnDestroy {
     console.log("$event :", $event);
     console.log("search change :");
   }
+
+  getRandomColor() {
+    var color = Math.floor(0x1000000 * Math.random()).toString(16);
+    return '#' + ('000000' + color).slice(-6);
+  }
 }
